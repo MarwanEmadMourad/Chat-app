ad800e0cae551699eda880f0eb3dd2c2b8b5186c
MarwanEmadMourad <marwanemad36@gmail.com>
2022-08-01 23:25:32 +0200

    Setting up socket.io config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require("express")
 const path = require("path")
 const http = require("http")
-
+const socketio = require("socket.io")
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+
 const publicDirectioryPath = path.join(__dirname,"../public")
 
 app.use(express.static(publicDirectioryPath))
 
+io.on("connection" , () =>{
+    console.log("New client connection established.")
+})
 
-app.listen(5001,()=>{
-    console.log("App is running on port 5001.")
+server.listen(5001,()=>{
+    console.log("Server is running on port 5001.")
 })
